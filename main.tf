1695bff73c5c29f7a3f734d675742207dc2dedbe
chengkai liang <chengkli@cisco.com>
2023-02-23 13:11:33 -0800

    add environment variable and assign sandbox to it

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
 module "vpc" {
   source = "./modules/vpc/"  
+  environment = var.environment
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "environment" {
+  description = "a name of an environment, i.e. prod, sandbox, dev, ..."
+  type = string
+}
