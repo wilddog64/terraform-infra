29b34b4591d17024cb34fea0129f338c79569af1
chengkai liang <chengkli@cisco.com>
2023-02-23 11:19:10 -0800

    add main.tf and provider in root

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  backend "s3" {
+    bucket         = "chengkli-terraform"
+    key            = "state/terraform.tfstate"
+    region         = "us-west-2"
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
