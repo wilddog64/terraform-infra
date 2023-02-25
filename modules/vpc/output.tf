4d92c65ce710b395a6820b53e5ca9058626ff48a
chengkai liang <chengkli@cisco.com>
2023-02-24 16:56:26 -0800

    change output parameters to be pural

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -2,10 +2,10 @@ output "id" {
   value = aws_vpc.cloud.id
 }
 
-output "public_subnet_id" {
+output "public_subnet_ids" {
   value = [for k, v in aws_subnet.public: v.id]
 }
 
-output "private_subnet_id" {
+output "private_subnet_ids" {
   value = [for k, v in aws_subnet.private: v.id]
 }
