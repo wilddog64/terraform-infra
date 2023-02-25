0aa9459110b90b3a065180882f14a19746bd7619
chengkai liang <chengkli@cisco.com>
2023-02-24 16:58:26 -0800

    inital checkin for network-acl-inbound module

---
+++ modules/network-acl-inbound/main.tf
@@ -0,0 +1,50 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# SETUP NETWORK ACL RULES FOR INBOUND TRAFFIC
+# This template attaches rules to an existing network ACL that allow inbound connections on a given set of ports from
+# a given set of CIDR blocks. This includes both the inbound rules to allow the inbound connection and the outbound
+# rules to allow the response.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ---------------------------------------------------------------------------------------------------------------------
+# SET TERRAFORM RUNTIME REQUIREMENTS
+# ---------------------------------------------------------------------------------------------------------------------
+
+terraform {
+  # This module has been updated with 0.12 syntax, which means it is no longer compatible with any versions below 0.12.
+  required_version = ">= 0.12"
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE INBOUND RULES
+# Allow resources in this subnet to receive requests on the specified ports
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_network_acl_rule" "allow_inbound" {
+  count          = var.num_inbound_cidr_blocks
+  network_acl_id = var.network_acl_id
+  rule_number    = var.ingress_rule_number + (count.index * 5)
+  egress         = false
+  protocol       = var.protocol
+  rule_action    = "allow"
+  cidr_block     = element(var.inbound_cidr_blocks, count.index)
+  from_port      = var.inbound_from_port
+  to_port        = var.inbound_to_port
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CREATE THE OUTBOUND RULE FOR RETURN TRAFFIC
+# When a resource accepts a connection, it creates a new socket on a different, high numbered ("ephemeral") port to
+# send a response. Since network ACLs are stateless, we have to explicitly add these rules that allow "return" traffic.
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "aws_network_acl_rule" "allow_outbound_return" {
+  count          = var.num_inbound_cidr_blocks
+  network_acl_id = var.network_acl_id
+  rule_number    = var.egress_rule_number + (count.index * 5)
+  egress         = true
+  protocol       = var.protocol
+  rule_action    = "allow"
+  cidr_block     = element(var.inbound_cidr_blocks, count.index)
+  from_port      = var.ephemeral_from_port
+  to_port        = var.ephemeral_to_port
+}
---
+++ modules/network-acl-inbound/variables.tf
@@ -0,0 +1,63 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# MODULE PARAMETERS
+# These variables are expected to be passed in by the operator when calling this terraform module.
+# ----------------------------------------------------------------------------------------------------------------------
+
+variable "inbound_from_port" {
+  description = "Allow all inbound traffic on ports between var.inbound_from_port and var.inbound_to_port, inclusive"
+  type        = number
+}
+
+variable "inbound_to_port" {
+  description = "Allow all inbound traffic on ports between var.inbound_from_port and var.inbound_to_port, inclusive"
+  type        = number
+}
+
+variable "inbound_cidr_blocks" {
+  description = "A list of CIDR blocks from which inbound connections should be allowed to var.inbound_ports"
+  type        = list(string)
+}
+
+variable "num_inbound_cidr_blocks" {
+  description = "The number of CIDR blocks in var.inbound_cidr_blocks. We should be able to compute this automatically, but due to a Terraform limitation, we can't: https://github.com/hashicorp/terraform/issues/14677#issuecomment-302772685"
+  type        = number
+}
+
+variable "network_acl_id" {
+  description = "The id of the network ACL to which the new rules should be attached"
+  type        = string
+}
+
+variable "ingress_rule_number" {
+  description = "The starting number to use for ingress rules that are added. Each ingress rule in an network ACL must have a unique rule number."
+  type        = number
+}
+
+variable "egress_rule_number" {
+  description = "The number to use for the egress rule that will be added. Each egress rule in an network ACL must have a unique rule number."
+  type        = number
+}
+
+variable "protocol" {
+  description = "The protocol (e.g. TCP). If you set this value to -1 or 'all', any protocol and any port is allowed (so the from_port and to_port settings are ignored!)."
+  type        = string
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# DEFINE CONSTANTS
+# Generally, these values won't need to be changed.
+# ---------------------------------------------------------------------------------------------------------------------
+
+# See http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_ACLs.html#VPC_ACLs_Ephemeral_Ports
+variable "ephemeral_from_port" {
+  description = "Return traffic will be allowed on all ports between var.ephemeral_from_port and var.ephemeral_to_port, inclusive, from var.inbound_cidr_blocks"
+  type        = number
+  default     = 1024
+}
+
+# See http://docs.aws.amazon.com/AmazonVPC/latest/UserGuide/VPC_ACLs.html#VPC_ACLs_Ephemeral_Ports
+variable "ephemeral_to_port" {
+  description = "Return traffic will be allowed on all ports between var.ephemeral_from_port and var.ephemeral_to_port, inclusive, from var.inbound_cidr_blocks"
+  type        = number
+  default     = 65535
+}
