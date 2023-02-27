b7f976525aa5f82273cbb25d4abad20af6075f60
chengkai liang <chengkli@cisco.com>
2023-02-27 14:32:04 -0800

    add ssh-inbound-acl module

--- main.tf
+++ main.tf
@@ -15,3 +15,19 @@ module "bastion" {
     "216.9.31.182/32"
   ]
 }
+
+
+module "ssh-inbound-acl" {
+  source = "./modules/inblund-network-acl/"
+
+  vpc_id = module.vpc.id
+  vpc_name = "${var.environment}-${module.vpc.id}"
+  public_subnet_ids = module.vpc.public_subnet_ids
+  public_subnet_cidr_blocks = module.vpc.public_subnet_cidrs_blocks
+  ingress_rule_number = 100
+  egress_rule_number = 100
+  protocol = "tcp"
+  inbound_from_port = 22
+  inbound_to_port = 22
+}
+
